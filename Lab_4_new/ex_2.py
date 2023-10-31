a75eb85518fba810168f4c3b292918ab1e8a0076
Pal Alexandra <palalexandra3@yahoo.com>
2023-10-31 17:10:06 +0200

    lab_4 completed

--- Lab_4_new/ex_2.py
+++ Lab_4_new/ex_2.py
@@ -3,3 +3,39 @@
 # like push, pop, peek
 # (the last two methods should return None
 # if no element is present in the queue).
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def push(self, element):
+        self.queue.append(element)
+
+    def pop(self):
+        if len(self.queue) == 0:
+            return None
+        else:
+            return self.queue.pop(0)
+
+    def peek(self):
+        if len(self.queue) == 0:
+            return None
+        else:
+            return self.queue[0]
+
+queue = Queue()
+queue.push(1)
+queue.push('a')
+queue.push([2, 3])
+print("THE QUEUE")
+print(queue.queue)
+
+print("pop OPERATION")
+print(queue.pop())
+print("THE QUEUE AFTER pop OPERATION")
+print(queue.queue)
+
+print("peek OPERATION")
+print(queue.peek())
+print("THE QUEUE AFTER peek OPERATION")
+print(queue.queue)
---
+++ Lab_4_new/ex_3.py
@@ -0,0 +1,102 @@
+#Write a Python class that simulates a matrix of size NxM,
+# with N and M provided at initialization.
+# The class should provide methods to
+# access elements (get and set methods)
+# and some methematical functions such as transpose, matrix multiplication
+# and a method that allows iterating through all elements form a matrix an apply
+# a transformation over them (via a lambda function).
+import random
+
+
+class Matrix:
+
+    def __init__(self, N, M):
+        self.N = N
+        self.M = M
+        self.matrix = [[None for i in range(M)] for j in range(N)]
+
+    def get(self, i, j):
+        return self.matrix[i][j]
+
+    def set(self, i, j, value):
+        if i < self.N and j < self.M:
+            self.matrix[i][j] = value
+        else:
+            print("Index out of bounds")
+
+    def print(self):
+        for i in range(self.N):
+            for j in range(self.M):
+                print(self.get(i, j), end=" ")
+            print()
+
+    def transpose(self):
+        new_matrix = Matrix(self.M, self.N)
+        for i in range(self.N):
+            for j in range(self.M):
+                new_matrix.set(j, i, self.get(i, j))
+        return new_matrix
+
+    def multiply(self, other):
+        if self.M != other.N:
+            print("Matrices cannot be multiplied")
+            return None
+        else:
+            new_matrix = Matrix(self.N, other.M)
+            for i in range(self.N):
+                for j in range(other.M):
+                    sum = 0
+                    for k in range(self.M):
+                        if (isinstance(self.get(i, k), int) or isinstance(self.get(i, k), float)) and (isinstance(other.get(k, j), int) or isinstance(other.get(k, j), float)):
+                            sum += self.get(i, k) * other.get(k, j)
+                        else:
+                            print(self.get(i, k))
+                            print(other.get(k, j))
+                            print("Matrices cannot be multiplied. Not all elements are not numbers")
+                            return None
+                    new_matrix.set(i, j, sum)
+            return new_matrix
+
+    def apply(self, lambda_function):
+        for i in range(self.N):
+            for j in range(self.M):
+                self.set(i, j, lambda_function(self.get(i, j)))
+
+
+matrix1 = Matrix(2, 3)
+matrix2 = Matrix(3, 4)
+
+for i in range(matrix1.N):
+    for j in range(matrix1.M):
+        x = random.randint(0, 11)
+        matrix1.set(i, j, x)
+
+
+for i in range(matrix2.N):
+    for j in range(matrix2.M):
+        x = random.randint(11, 31)
+        matrix2.set(i, j, x)
+
+print("MATRIX 1")
+matrix1.print()
+print()
+
+print("MATRIX 2")
+matrix2.print()
+print()
+
+matrix3 = matrix1.transpose()
+print("MATRIX1 TRANSPOSED (aka matrix3)")
+matrix3.print()
+print()
+
+
+matrix4 = matrix1.multiply(matrix2)
+print("MATRIX1 MULTIPLIED BY MATRIX2 (aka matrix4)")
+matrix4.print()
+print()
+
+matrix4.apply(lambda t: t + t % 2)
+print("MATRIX4 AFTER APPLYING LAMBDA FUNCTION")
+matrix4.print()
+print()
